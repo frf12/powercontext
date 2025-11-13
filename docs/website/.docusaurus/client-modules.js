4fab27a2708cc64ab858bb7a5bffa9da3db749c1
Teingi <jingshun.tq@oceanbase.com>
2025-11-13 18:59:55 +0800

    fix docs

---
+++ docs/website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/jingshun.tq/project/powermem/docs/website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/jingshun.tq/project/powermem/docs/website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/jingshun.tq/project/powermem/docs/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/jingshun.tq/project/powermem/docs/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/jingshun.tq/project/powermem/docs/website/src/css/custom.css"),
+];
